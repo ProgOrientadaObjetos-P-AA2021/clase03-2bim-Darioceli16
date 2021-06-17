803423ace503b1b742d733972b5958373d2b67e6
Darioceli16 <dc122016@gmail.com>
2021-06-16 19:07:39 -0500

    EjemploHerencia

--- EjemploHerencia/Herencias/src/Reporte/Ejecutor.java
+++ EjemploHerencia/Herencias/src/Reporte/Ejecutor.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 public class Ejecutor {
 
     public static void main(String[] args) {
-
         Estudiante est1 = new Estudiante("René", "Elizalde", 33, 100.2);
         Docente doc1 = new Docente("José", "Hincapié", 40, 1000);
         Policia pol1 = new Policia("María", "MENDOZA", 35, "Cabo");
@@ -25,27 +24,31 @@ public class Ejecutor {
         Docente doc2 = new Docente("Luis", "Alvarez", 38, 1000);
         Policia pol2 = new Policia("ALEX", "Medina", 33, "Cabo");
 
-        ArrayList listaEstudiante = new ArrayList<>();
-        listaEstudiante.add(est1);
-        listaEstudiante.add(est2);
-
         ArrayList listaDocente = new ArrayList<>();
         listaDocente.add(doc1);
         listaDocente.add(doc2);
+
+        ArrayList listaEstudiante = new ArrayList<>();
+        listaEstudiante.add(est1);
+        listaEstudiante.add(est2);
 
         ArrayList listaPolicia = new ArrayList<>();
         listaPolicia.add(pol1);
         listaPolicia.add(pol2);
 
-        ReporteDocentes rd = new ReporteDocentes("0001Doc");
+        ReporteDocentes rd = new ReporteDocentes("001Doc");
+        rd.listaDocente = listaDocente;
 
-        ReporteEstudiantes re = new ReporteEstudiantes("0001Est");
+        ReporteEstudiantes re = new ReporteEstudiantes("001Est");
+        re.listaEstudiante = listaEstudiante;
 
-        ReportePolicias rp = new ReportePolicias("0001Pol");
+        ReportePolicias rp = new ReportePolicias("001Pol");
+        rp.listaPolicia = listaPolicia;
 
         System.out.println(rd);
         System.out.println(re);
         System.out.println(rp);
 
     }
+
 }
